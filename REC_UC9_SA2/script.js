1e07acf8719e37b14325f5cb1b477de8bae4e675
Victor Fernandes Garcia <63003337+victor-ce@users.noreply.github.com>
2022-11-22 19:57:38 -0300

    Add files via upload

---
+++ REC_UC9_SA2/script.js
@@ -0,0 +1,10 @@
+
+
+function enviarEmail(event){
+
+    let email = document.getElementById("email").value;
+    let senha = document.getElementById("senha").value;
+    console.log(email)
+    console.log(senha)
+}
+
